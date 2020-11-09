30d44d98fc65488ecb9fe8755c84268fb28864f2
midnite_runr <secretsquirrel@users.noreply.github.com>
2020-11-09 09:56:46 -0500

    Update sigthief.py

--- sigthief.py
+++ sigthief.py
@@ -214,7 +214,7 @@ def signfile(exe, sigfile, output):
 
 if __name__ == "__main__":
     usage = 'usage: %prog [options]'
-    print("!! Like this? Sponsor here: https://github.com/sponsors/secretsquirrel")
+    print("!! Like this? Sponsor here: https://github.com/sponsors/secretsquirrel\n\n")
     parser = OptionParser()
     parser.add_option("-i", "--file", dest="inputfile", 
                   help="input file", metavar="FILE")
